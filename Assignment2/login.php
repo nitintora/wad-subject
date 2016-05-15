d67beda66bf03851680d582107175df51449d8b1
Nitin <nitintoora@gmail.com>
2016-05-15 09:37:00 +0000

    Register and Login - Assignment2

--- Assignment2/login.php
+++ Assignment2/login.php
@@ -1,42 +1,12 @@
-<!--  Author : Nitin Tora
-Student Number: 100649911
-Login page for existing customers  -->
-
-<html lang="en">
-	<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<meta charset="UTF-8">
-	<meta name="description" content="Web Application Development - Assignment 1">
-	<meta name="keywords" content="Html, php, Mysql">
-	<title>Cabs Online | Book the cab now| Login</title>
-	</head>
-	<h1> Shop Online</h1>
-	<nav>
-    <a href="/html/">HTML</a> |
-    <a href="/css/">CSS</a> |
-    <a href="/js/">JavaScript</a> |
-    <a href="/jquery/">jQuery</a>
-    </nav>
-	
-	<p> Please fill the form to register</p>
-	<form method="GET" action = "">
-	Email: <input type="text" name="email" required = "required"/> <br />
-	Password: <input type="password" name="password" required="required"/> <br />
-	<!-- <input type="hidden" name="submitted" value="true" /> -->
-	<input type="submit" value="Login" name="submitted" /><br />
-	</form>
-	<p> <b>New Member? <a href =register.php>Register now</a></b></p>
-</html>
 
 <?php
-if(($_GET['email'] != null && $_GET['password'] != null))
-{
-$xmlFile = "customer.xml";
- $HTML = "";
- $count = 0;
+//login
+error_log("====inside login.php=====");
+session_start();
+$xmlFile = "customer1.xml";
  //$dt = simplexml_load_file($xmlFile);
- $dom = DOMDocument::load($xmlFile);
- $customer = $dom->getElementsByTagName("customer"); 
+$dom = DOMDocument::load($xmlFile);
+$customer = $dom->getElementsByTagName("customer"); 
  
 foreach($customer as $node) 
 { 
@@ -46,14 +16,30 @@ foreach($customer as $node)
 	 $password = $node->getElementsByTagName("Password");
 	 $password = $password->item(0)->nodeValue;
 
-	if (($email == $_GET["email"]) && ($password == $_GET["password"]) )
+	if (($email == trim($_GET["email"])) && ($password == trim($_GET["password"])) )
 	{
 	$_SESSION['email'] = $email;
-  	header('location: bidding.php');
+	$flag = true;
 	break;
 	}
-	
+	else {
+		$flag = false;
+	}
 }
 
-echo "Credentials not found! Try Again";
+	$response = array();
+	if($flag == true)
+	{
+		$response = array("login"=>"ok"); 
+		error_log("=====in true condition");
+		echo  "ok";
+		//echo "hello";
+	}
+	else {
+		$response = array("login"=>"fail");
+		//echo json_encode($response);
+		echo "fail";
 }
+
+
+?>
---
+++ register.php
@@ -0,0 +1,82 @@
+<?php
+
+//register
+$xml = new DOMDocument('1.0');
+$xml->preserveWhiteSpace = false;
+$xml->formatOutput = true;
+$xml->load('customer1.xml');
+$xmlFile = "customer1.xml";
+ //$dt = simplexml_load_file($xmlFile);
+$dom = DOMDocument::load($xmlFile);
+if(!xml)
+{
+	$customers= $xml->createElement('customers');
+	$xml->appendChild($customers);
+}
+else 
+{
+	$customers = $xml->firstChild;	
+}
+//customer element
+$customer = $dom->getElementsByTagName("customer"); 
+	
+$emailreg = $_GET['emailreg'];
+$fname = $_GET['fname'];
+$surname = $_GET['surname'];
+$custid = uniqid();
+$password = md5(uniqid());
+
+foreach($customer as $node) 
+{
+	 $emailCheck = $node->getElementsByTagName("Email");
+	 $emailCheck = $emailCheck->item(0)->nodeValue;
+
+	if ($emailCheck == $emailreg)
+	{
+		$flag = 1;
+		break;
+	}
+}
+//name element
+if($flag==1)
+{
+	echo "emailexists";
+}
+else
+{
+$customer = $xml->createElement("customer");
+$customers->appendChild($customer);
+
+//password element
+$fnamexml = $xml->createElement("Firstname", $fname);
+$customer->appendChild($fnamexml);
+
+//First Name
+$surnamexml = $xml->createElement("Surname", $surname);
+$customer->appendChild($surnamexml);
+
+//surname
+$emailxml = $xml->createElement("Email", $emailreg);
+$customer->appendChild($emailxml);
+
+//id
+$idxml = $xml->createElement("id", $custid);
+$customer->appendChild($idxml);
+
+//id
+$passwordxml = $xml->createElement("Password", $password);
+$customer->appendChild($passwordxml);
+	
+$recipient = $_GET['emailreg'];
+$subject = "Welcome to ShopOnline!";
+$message = "Dear" .$fname. "Welcome to ShopOnline! Your Customer id is" .$custid. "and the password is:" .$password;
+$header = "From registration@shoponline.com.au";
+mail($recipient, $subject, $message, $header, "-r 100649911@student.swin.edu.au"); 
+//echo ""
+
+}
+
+//echo "<xmp>" .$xml->saveXML(). "</xmp>";
+$xml->save("customer1.xml");
+
+?>
